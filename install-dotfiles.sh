1a2aaf72bcc5523e761a7572e079001e244d662b
garaiza-93 <araizagustavo93@gmail.com>
2022-11-03 00:19:29 -0500

    add pseudocode for install script

---
+++ install-dotfiles.sh
@@ -0,0 +1,7 @@
+#steps to take
+#1. install tmux
+#2. install zsh
+#3. sudo echo 'export ZDOTDIR="$HOME/.config/zsh"' > /etc/zsh/zshenv
+#4. ZSH="$HOME/.config/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#5. clone repo, but bare. see https://www.atlassian.com/git/tutorials/dotfiles
+#6. if all goes well, repo files should go in their right spot!
