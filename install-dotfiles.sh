f1461c1bb274cd61a5def8d440aa5b37bdeadb94
garaiza-93 <araizagustavo93@gmail.com>
2022-11-14 10:31:11 -0600

    remove oh-my-zsh from script, preferring minimal zsh

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -72,6 +72,5 @@ cd ..
 
 #3. install zsh
 #4. sudo echo "export ZDOTDIR="$HOME/.config/zsh"" > /etc/zsh/zshenv
-#5. ZSH="$HOME/.config/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-#6. clone repo, but bare. see https://www.atlassian.com/git/tutorials/dotfiles
-#7. if all goes well, repo files should go in their right spot!
+#5. clone repo, but bare. see https://www.atlassian.com/git/tutorials/dotfiles
+#6. if all goes well, repo files should go in their right spot!
